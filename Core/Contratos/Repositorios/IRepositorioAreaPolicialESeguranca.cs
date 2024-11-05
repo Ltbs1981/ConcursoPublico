2d75f1bc530ad83a982375a78cd86f59bf005d8a
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-11-05 12:02:22 -0300

    set up entidy and interfaces

---
+++ Core/Contratos/Repositorios/IRepositorioAreaPolicialESeguranca.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcursoPublico.Core.Contratos.Repositorios
+{
+    internal interface IRepositorioAreaPolicialESeguranca
+    {
+    }
+}
---
+++ Core/Entidades/AreaControleEGestao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcursoPublico.Core.Entidades
+{
+    internal class AreaControleEGestao
+    {
+        public string Concurso { get; set; }
+        public string Banca { get; set; }
+        public string PeriodoInscricoes { get; set; }
+        public DateTime DataProva { get; set; }
+        public int Vagas { get; set; }
+        public decimal Salario { get; set; }
+    }
+}
